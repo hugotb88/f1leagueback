9f7840cb45d61acb38e4a8260c43c630c2fd2b6b
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-03-13 15:03:57 -0600

    First Commit

---
+++ src/main/java/com/projects/f1leagueback/F1leaguebackApplication.java
@@ -0,0 +1,13 @@
+package com.projects.f1leagueback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1leaguebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1leaguebackApplication.class, args);
+    }
+
+}
